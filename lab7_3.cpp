34391592094f8a1e85566217cf47b95fc93a98d0
Ngern <sattawatwiwattana@gmail.com>
2025-12-17 00:23:45 +0700

    commit message

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,19 +1,30 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-char before(char x){
-	//Write your function definition here
+char before(char c)
+{
+   
+    if (c >= 'A' && c <= 'Z')
+    {
+        if (c == 'A')
+            return 'Z';
+        else
+            return c - 1;
+    }
+    
+    return '0';
 }
 
-int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
+int main()
+{
+    cout << before('A') << endl; 
+    cout << before('B') << endl; 
+    cout << before('P') << endl; 
+    cout << before('T') << endl; 
+    cout << before('Z') << endl; 
+    cout << before('a') << endl; 
+    cout << before('0') << endl; 
+    cout << before('c') << endl; 
+
+    return 0;
 }
